354e4245a17ca4d871a8bda3255089969103f6bb
Sravani Gandham <sravani_gandham@apple.com>
2025-04-22 20:19:53 +0530

    add function to read text file

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String textFileUrl = "https://courses.cs.washington.edu/courses/cse390c/22sp/lectures/moby.txt";
+
+        try {
+            URL url = new URL(textFileUrl);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Error reading from URL: " + e.getMessage());
+        }
+    }
+
+}
